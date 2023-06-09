c46a685e9b5374cd6ab75170f1d2fa4449360744
[Gaia <[dzhayadzoy@gmail.com]>
2023-06-09 13:15:34 +0500

    created example_4

---
+++ Example_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine( )!;
+if (username.ToLower() == "Gaia")
+{
+    Console.Write("I have been waiting for you, Gaia");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine (username);
+}
